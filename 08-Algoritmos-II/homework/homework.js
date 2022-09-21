d57802b865ac58205d239d6c34eb8235a8963f10
br1an17 <106201381+br1an17@users.noreply.github.com>
2022-09-21 16:43:14 -0300

    algoritmos 2

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,26 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let pivot = array.pop();
+  let arrayi = [];
+  let arrayd = [];
+  let final = [];
+
+
+  for (let i = 0; i < array.length; i++)
+    if (array[i] <= pivot) {
+      arrayi.push(array[i])
+    } else {
+      arrayd.push(array[i])
+    }
+  return final.concat(quickSort(arrayi), pivot, quickSort(arrayd));
+
+
+
 }
 
 function mergeSort(array) {
@@ -15,7 +35,49 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+
+
+
+  if (array.length <2) {
+    return array;
+      }
+
+      var mitad = Math.floor(array.length/2);
+
+      var left = array.slice(0,mitad)
+      var right = array.slice(mitad)
+
+
+      return marge(mergeSort(left),mergeSort(right));}
+
+
+function marge(left,right) {
+
+   var final=[];
+   var  iz =0;
+   var  der =0;
+
+   while(iz < left.length && der < right.length) {
+
+   if (left[iz] < right[der]) {
+
+     final.push(left[iz++]);
+   
+}else{
+
+final.push(right[der++]);
+
+ }
 }
+
+return final.concat(left.slice(iz)).concat(right.slice(der))}
+ 
+
+
+
+
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
